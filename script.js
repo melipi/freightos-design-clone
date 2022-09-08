47ee66faeb3c292b82c1c183d3cefcd03e088ae8
melipi <melissapita@gmail.com>
2022-09-07 21:26:18 -0400

    starting to implement dropdown menus

---
+++ script.js
@@ -0,0 +1,7 @@
+// Open dropdowns
+const originDropdown = document.querySelector('.origin');
+
+originDropdown.addEventListener('click', e => {
+    e.preventDefault();
+    console.log('Clicked originDropdown!');
+})
