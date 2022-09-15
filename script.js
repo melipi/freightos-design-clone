1ef310920a10c59f0ff8cb19b1b0842d55da9a8a
melipi <melissapita@gmail.com>
2022-09-15 14:41:06 -0400

    refactored code to use event delegation

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const originSearch = document.querySelector("#origin-search");
-const originDropdown = document.querySelector("#origin-dropdown");
+// // DOM
+// const originSearch = document.querySelector("#origin-search");
+// const originDropdown = document.querySelector("#origin-dropdown");
 
-originSearch.addEventListener("click", () => {
-    originDropdown.classList.toggle("hide");
-});
+// // Dropdown toggler
+// originSearch.addEventListener("click", () => {
+//     originDropdown.classList.toggle("hide");
+// });
 
-window.addEventListener("click", (e) => {
-    if (!originSearch.contains(e.target)) {
-        originDropdown.classList.add("hide");
+// // Close dropdown if user clicks outside of button or dropdown
+// window.addEventListener("click", (e) => {
+//     if (!originSearch.contains(e.target) && !originDropdown.contains(e.target)) {
+//         originDropdown.classList.add("hide");
+//     }
+// });
+
+// Event Delegation Version
+const searchBar = document.querySelector(".search-bar");
+const searchSection = document.querySelector(".search-section");
+
+searchBar.addEventListener("click", e => {
+    if(e.target.className === "search-section") {
+        e.preventDefault();
+        e.target.nextElementSibling.classList.toggle("hide");
     }
 });
 
