6c3d3771425b4fd79ee2ecc2f350e913d753ba69
melipi <melissapita@gmail.com>
2022-09-15 13:54:21 -0400

    completed origin dropdown toggle interaction

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ originSearch.addEventListener("click", () => {
     originDropdown.classList.toggle("hide");
 });
 
-window.onClick = e => {
-    if(!e.target.matches("#origin-search")) {
-        console.log("outside click");
-        originDropdown.classList.toggle("hide");
+window.addEventListener("click", (e) => {
+    if (!originSearch.contains(e.target)) {
+        originDropdown.classList.add("hide");
     }
-}
+});
 
 // Choices.js dropdown menus
 
