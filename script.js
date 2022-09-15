6b3f0b135371aeaaf74d708186d03d567076bfdb
melipi <melissapita@gmail.com>
2022-09-15 13:17:23 -0400

    first test of toggle dropdown

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+const originSearch = document.querySelector("#origin-search");
+const originDropdown = document.querySelector("#origin-dropdown");
+
+originSearch.addEventListener("click", () => {
+    originDropdown.classList.toggle("hide");
+});
+
+window.onClick = e => {
+    if(!e.target.matches("#origin-search")) {
+        console.log("outside click");
+        originDropdown.classList.toggle("hide");
+    }
+}
+
 // Choices.js dropdown menus
 
